2883bf9249eaaba0ee188e85532f644a172d8ee9
KidKoder365 <kidai@aim.com>
2019-10-23 03:30:42 -0400

    completed code for Area of a cirle with radius given by user

---
+++ Area/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a radius: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            while (radius < 0)
+            {
+                Console.WriteLine("You have enter an invalid entry.  Please enter a positive number.");
+                Console.Write("Please enter a positive entry for your radius: ");
+                radius = double.Parse(Console.ReadLine());
+            }
+
+
+                double area = 3.14 * radius * radius;
+                Console.WriteLine($"The area of your cirle is {area}.");
+                Console.ReadLine();
+            
+            
+        }
+    }
+}
